e1db6ea43c103b8047275069b1f09f46c2610b42
Georgy Lukyanov <georgiylukjanov@gmail.com>
2019-03-02 00:47:01 +0000

    Remove unproven theorems.

--- src/Control/Selective/Rigid.v
+++ src/Control/Selective/Rigid.v
@@ -16,8 +16,6 @@ Inductive Select (f : Type -> Type) (a : Type) :=
 Arguments Pure {f} {a}.
 Arguments MkSelect {f} {a} {b}.
 
-Check Select_ind.
-
 Fixpoint Select_map {A B : Type} {F : Type -> Type} `{Functor F}
            (f : (B -> A)) (x : Select F B) : Select F A :=
   match x with
@@ -30,8 +28,6 @@ Program Instance Select_Functor (F : Type -> Type)
   fmap := fun _ _ f x => Select_map f x
 }.
 
-Definition undefined {a : Type} : a. Admitted.
-
 Definition law3_f {A B C : Type}
            (x : B + C) : B + (A + C) := Right <$> x.
 
@@ -39,18 +35,12 @@ Definition law3_g {A B C : Type}
            (y : A + (B -> C)) : B -> A * B + C :=
   fun a => Either_bimap (fun p => pair p a) (fun f => f a) y.
 
-(* Fixpoint law3_g'  {A B C : Type} {F : Type -> Type} *)
-(*            (v : Select F (A + (B -> C))) : Select F (B -> (A * B + C)) := *)
-(*   match v with *)
-(*            | Pure y => Pure (fun a => (Either_bimap (fun p => pair p a) (fun f => f a) y)) *)
-(*            | MkSelect x y => MkSelect (law3_g' x) y *)
-(*            end. *)
 Definition law3_h  {A B C : Type}
            (f : A -> B -> C) : A * B -> C :=
   fun p => match p with
            | pair x y => f x y
            end.
-
+(* ?O(n)? select implementation *) 
 Fixpoint Select_select_go {A B C : Type} {F : Type -> Type} `{Functor F}
          (x : Select F (A + B)) (s : Select F C) (k : C -> (A -> B)) : Select F B :=
   match s with
@@ -77,14 +67,6 @@ Program Instance Select_Applicative
   ; pure _ x := Pure x
   ; ap _ _ f x := Select_ap f x
 }.
-
-
-Fixpoint Select_depth {A : Type} {F : Type -> Type} `{Functor F}
-           (x : Select F A) : nat :=
-  match x with
-  | Pure y => 0
-  | MkSelect y z => 1 + Select_depth y
-  end.
 
 Program Instance Select_Selective (F : Type -> Type) `{Functor F}: Selective (Select F) :=
   { is_applicative := Select_Applicative F
@@ -137,7 +119,7 @@ Proof.
       * destruct s.
         -- simpl. unfold Select_ap.
 
-Theorem validation_selective_law3_assoc
+Theorem select_selective_law3_assoc
   {A B C : Type} {F : Type -> Type} `{Functor F}
   (x : Select F (B + C))
   (y : Select F (A + (B -> C)))
@@ -157,57 +139,4 @@ Proof.
   (*     destruct s0; *)
   (*     simpl; reflexivity. *)
 Admitted.
-(******************** Selective theorems *****************************)
 
-(* -- Apply a pure function to the result *)
-(* f <$> select x y = select (fmap f <$> x) (fmap f <$> y) *)
-Theorem validation_selective_theorem1
-  {A B C M : Type} `{Monoid M}
-  (f : A -> C) (x : Validation M (B + A)) (y : Validation M (B -> A)) :
-  f <$> select x y = select (fmap f <$> x) ((fun g => compose f g) <$> y).
-Proof.
-  destruct x.
-  - simpl. reflexivity.
-  - destruct s;
-    destruct y;
-    simpl; reflexivity.
-Qed.
-(* -- Apply a pure function to the Left case of the first argument *)
-(* select (first f <$> x) y = select x ((. f) <$> y) *)
-Theorem validation_selective_theorem2
-  {A B C M : Type} `{Monoid M}
-  (f : A -> B) (x : Validation M (A + C)) (y : Validation M (B -> C)) :
-  select (mapLeft f <$> x) y = select x ((fun g => compose g f) <$> y).
-Proof.
-  destruct x.
-  - destruct y;
-    simpl; reflexivity.
-  - destruct y;
-    destruct s;
-    simpl; reflexivity.
-Qed.
-(* -- Apply a pure function to the second argument *)
-(* select x (f <$> y) = select (first (flip f) <$> x) (flip ($) <$> y) *)
-Theorem validation_selective_theorem3
-  {A B C M : Type} `{Monoid M}
-  (f : A -> B -> C) (x : Validation M (B + C)) (y : Validation M A) :
-  select x (f <$> y) = select (mapLeft (flip f) <$> x) ((fun y f => f y) <$> y).
-Proof.
-  destruct x.
-  - destruct y;
-    simpl; reflexivity.
-  - destruct y;
-    destruct s; simpl; reflexivity.
-Qed.
-(* -- Generalised identity *)
-(* x <*? pure y = either y id <$> x *)
-Theorem validation_selective_theorem4
-  {A B M : Type} `{Monoid M}
-  (x : Validation M (A + B)) (y : A -> B) :
-  x <*? pure y = either y id <$> x.
-Proof.
-  destruct x.
-  - simpl.
-    reflexivity.
-  - destruct s; simpl; reflexivity.
-Qed.
