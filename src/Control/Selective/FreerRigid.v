143b5f92567134c2216bf10b69f6c0ce33f8247c
Georgy Lukyanov <georgiylukjanov@gmail.com>
2019-06-10 23:54:07 +0100

    More attempts to prove applicative laws.

--- src/Control/Selective/FreerRigid.v
+++ src/Control/Selective/FreerRigid.v
@@ -51,8 +51,7 @@ Select_map f (Pure a) => Pure (f a);
 Select_map f (MkSelect x y) =>
   MkSelect (Select_map (Either_bimap (fun k : _ -> A => f \o k) f) x) y.
 
-Program Instance Select_Functor (F : Type -> Type)
-  `{Functor F} : Functor (Select F) := {
+Program Instance Select_Functor (F : Type -> Type) : Functor (Select F) := {
   fmap := fun _ _ f x => Select_map f x
 }.
 
@@ -90,7 +89,7 @@ Proof.
   destruct x; trivial.
 Qed.
 
-Program Instance Select_FunctorLaws `{FunctorLaws F} : FunctorLaws (Select F).
+Program Instance Select_FunctorLaws : FunctorLaws (Select F).
 (* Theorem Select_Functor_law1 {A : Type} *)
 (*         `{Functor F} `{FunctorLaws F} : *)
 (*   forall (x : Select F A), fmap id x = id x. *)
@@ -129,34 +128,33 @@ induction x.
     simpl. rewrite Heqp. rewrite Heqq. rewrite Heqr.
     destruct y; trivial.
   }
-  rewrite <- H1.
+  rewrite <- H.
   now rewrite IHx.
 Qed.
 
-(* This is a halper function used in the Select_selectBy definition *)
+(* This is a helper function used in the Select_selectBy definition *)
 Definition g {A B C D E : Type}
            (f : A -> ((B -> C) + C))
            (a : A) :
-  Either (((D -> B) + B) -> ((D -> C) + C))
-         (E + C) :=
+  (((D -> B) + B) -> ((D -> C) + C)) + (E + C) :=
   match f a with
   | inr r => Right (Right r)
   | inl l => Left  (Either_bimap (fun k => l \o k) l)
   end.
 
-Equations Select_selectBy {A B C : Type} `{Functor F}
+Equations Select_selectBy {A B C : Type} {F : Type -> Type}
           (f : A -> ((B -> C) + C))
           (x : Select F A)
           (a : Select F B) : Select F C :=
 Select_selectBy f x (Pure y)       := (either (fun k => k y) id \o f) <$> x;
 Select_selectBy f x (MkSelect y z) := MkSelect (Select_selectBy (g f) x y) z.
 
-Definition Select_ap {A B : Type} `{Functor F}
+Definition Select_ap {A B : Type} {F : Type -> Type}
            (f : Select F (A -> B)) (x : Select F A) : Select F B :=
   Select_selectBy Left f x.
 
 Program Instance Select_Applicative
-        (F : Type -> Type) `{Functor F} : Applicative (Select F) :=
+        (F : Type -> Type) : Applicative (Select F) :=
   { is_functor := Select_Functor F
   ; pure _ x := Pure x
   ; ap _ _ f x := Select_ap f x
@@ -183,20 +181,145 @@ Lemma id_is_unique :
   forall (A : Type) (f : A -> A), f = id.
 Admitted.
 
+Require Import Coq.Program.Equality.
+Print JMeq.
+
+Lemma id_is_unique_2 :
+  forall (A B : Type) (f : A -> A) (g : B -> B),
+  f ~= g.
+Proof.
+  intros A B f g.
+  rewrite (id_is_unique A f).
+  rewrite (id_is_unique B g).
+Admitted.
+
+Lemma subst_id :
+  forall (A B : Type) (body : (A -> A) -> B),
+  (fun f : A -> A => body f) = const (body id).
+Proof.
+  intros A B body.
+  extensionality f.
+  now rewrite (@id_is_unique A f).
+Qed.
+
+Lemma eta_expand :
+  forall (A B : Type) (body : A -> B),
+  body = (fun arg : A => body arg).
+Proof. trivial. Qed.
+
+(* Lemma left_id_is_id {A : Type} : *)
+(*   forall (f : A -> A) (x : (A -> A) + A), *)
+(*   x = inl (B := A) f -> either (fun y => f y) id = id. *)
+
+Theorem Select_Applicative_law1 {F : Type -> Type} :
+  (* forall (A : Type), *)
+  (fun A : Type => Select_ap (F:=F) (Pure (@id A))) = (fun A : Type => @id (Select F A)).
+Proof.
+  unfold Select_ap.
+  extensionality A.
+  extensionality x.
+  revert A x.
+  (* Set Printing Implicit. *)
+  induction x as [| A X y IH z]; trivial;
+  rewrite Select_selectBy_equation_2.
+  rewrite id_x_is_x.
+  f_equal.
+  rewrite <- id_x_is_x.
+  rewrite <- IH.
+  remember (g inl) as p.
+  remember inl as q.
+  rewrite eta_expand in Heqp.
+  rewrite subst_id in Heqp.
+  unfold g in Heqp.
+  rewrite bimap_id in Heqp.
+  rewrite eta_expand in Heqq.
+  rewrite subst_id in Heqq.
+  rewrite Heqp.
+  rewrite Heqq.
+  clear IH p Heqp q Heqq.
+  Set Printing Implicit.
+
 Program Instance Select_ApplicativeLaws `{FunctorLaws F} : ApplicativeLaws (Select F).
 Obligation 1.
   extensionality x.
   unfold Select_ap.
+  revert F H H0 a x.
+  Set Printing Implicit.
+  induction x.
+  - trivial.
+  - rewrite Select_selectBy_equation_2.
+    rewrite id_x_is_x.
+    f_equal.
+    rewrite <- id_x_is_x.
+    rewrite <- IHx.
+    remember (g inl) as p.
+    remember inl as q.
+    assert (p id = q id).
+    { rewrite Heqp. rewrite Heqq. unfold g. now rewrite bimap_id. }
+    rewrite eta_expand in Heqp.
+    rewrite subst_id in Heqp.
+    rewrite eta_expand in Heqq.
+    rewrite subst_id in Heqq.
+    unfold g in Heqp.
+    rewrite bimap_id in Heqp.
+    rewrite Heqp.
+    rewrite Heqq.
+    Unset Printing Implicit.
+
+
+Program Instance Select_ApplicativeLaws `{FunctorLaws F} : ApplicativeLaws (Select F).
+Obligation 1.
+  extensionality x.
+  unfold Select_ap.
+  revert a x.
   induction x; trivial.
   - rewrite Select_selectBy_equation_2.
     rewrite id_x_is_x.
     f_equal.
+    rewrite <- id_x_is_x.
+    rewrite <- IHx.
+    remember (g inl) as p.
+    remember inl as q.
+    assert (p id = q id).
+    { rewrite Heqp. rewrite Heqq. unfold g. now rewrite bimap_id. }
+    rewrite eta_expand in Heqp.
+    rewrite subst_id in Heqp.
+    rewrite eta_expand in Heqq.
+    rewrite subst_id in Heqq.
+    unfold g in Heqp.
+    rewrite bimap_id in Heqp.
+    rewrite Heqp.
+    rewrite Heqq.
+    Set Printing Implicit.
+    reflexivity.
+    rewrite subst_id in Heqp.
+    assert (Either_bimap (Y:=A) (fun k : X -> A => id \o k) id = id).
+    { extensionality y.
+      destruct y; trivial. }
+    rewrite H1 in Heqp.
+    rewrite Heqp.
+    remember inl as q.
+    (* assert (t = (fun b : (X -> A) + A -> (X -> A) + A => inl b)).  *)
+    assert (Either_bimap (Y:=A) (fun k : X -> A => id \o k) id = id).
+    { extensionality y.
+      destruct y; trivial. }
+    assert (fun (a : A -> A) => Either_bimap (fun k : X -> A => a \o k) a = a).
+    { extensionality y.
+      destruct y; trivial. }
+    Check (g inl).
+    unfold g.
+    Check (fun a : A -> A => Either_bimap (fun k : X -> A => a \o k) a).
+
+    assert (forall X Y, inl = (fun (x : X) => @inl X Y x)).
+    { reflexivity. }
+    rewrite H1 in IHx.
+    remember ((fun a : A -> A => inl (Either_bimap (fun k : X -> A => a \o k) a))) as t.
     (* Looks like we're stuck; the gool here looks like this: *)
     (* IHx : Select_selectBy inl (Pure id) x = id x *)
     (* ============================ *)
     (* Select_selectBy (g inl) (Pure id) x = x *)
     (* We need to prove the following assertion: *)
-    assert (g inl = inl).
+    (* assert (g inl = inl). *)
     (* But it doesn't typecheck *)
 Admitted.
 Obligation 2.
@@ -206,10 +329,13 @@ Admitted.
 Obligation 5.
 extensionality x.
 unfold Select_ap.
+revert a b f x.
 induction x; trivial.
 - rewrite Select_selectBy_equation_2.
   rewrite Select_map_equation_2.
   f_equal.
+  remember (Either_bimap (fun k : X -> A => f \o k) f) as t.
+  rewrite <- (IHx ((Either_bimap (fun k : X -> A => f \o k) f))). 
 Obligation 4.
 Admitted.
 (* Proof. *)
