26bb7039e776e5add35e8412b0195dc69dcd3103
Georgy Lukyanov <georgiylukjanov@gmail.com>
2019-06-22 11:23:36 +0100

    Work on free rigid selective

--- src/Control/Selective/RigidFunction.v
+++ src/Control/Selective/RigidFunction.v
@@ -16,14 +16,66 @@ Inductive Select (F : Type -> Type) (A : Type) :=
 Arguments Pure {F} {A}.
 Arguments MkSelect {F} {A} {B}.
 
-
-Fixpoint Select_map {A B : Type} {F : Type -> Type} `{Functor F}
+Function Select_map {A B : Type} `{Functor F}
          (f : A -> B) (x : Select F A) : Select F B :=
   match x with
   | Pure a => Pure (f a)
   | MkSelect x y => MkSelect (Select_map (fmap f) x)
-                            (fmap (fun k : _ -> A => f \o k) y)
+                             (fmap (fun k : _ -> A => f \o k) y)
   end.
+
+Lemma Select_map_equation_1 :
+  forall (A B : Type) `(Functor F)
+  (f : A -> B) (a : A),
+    Select_map f (Pure a) = Pure (f a).
+Proof. trivial. Qed.
+
+Lemma Select_map_equation_2 :
+  forall (A B X : Type) `(Functor F)
+  (f : A -> B) (x : Select F (X + A)) (y : F (X -> A)),
+    Select_map f (MkSelect x y) = MkSelect (Select_map (fmap f) x)
+                                           (fmap (fun k : _ -> A => f \o k) y).
+Proof. trivial. Qed.
+
+Program Instance Select_Functor `{Functor F} : Functor (Select F) := {
+  fmap := fun _ _ f x => Select_map f x
+}.
+
+Import FunctorLaws.
+
+Program Instance Select_FunctorLaws `{FunctorLaws F} : FunctorLaws (Select F).
+(* Theorem Select_Functor_law1 {A : Type} *)
+(*         `{Functor F} `{FunctorLaws F} : *)
+(*   forall (x : Select F A), fmap id x = id x. *)
+Obligation 1.
+unfold id.
+extensionality x.
+generalize dependent x.
+generalize dependent a.
+induction x; trivial.
+rewrite Select_map_equation_2.
+assert (forall A, (fun x0 : A => x0) = id) as H_subst_id.
+{ reflexivity. }
+f_equal; repeat rewrite H_subst_id in *; rewrite fmap_id.
+- now rewrite IHx.
+- now unfold id.
+Qed.
+(* Theorem Select_Functor_law2 {A B C : Type} *)
+(*         `{Functor F} `{FunctorLaws F} : *)
+(*   forall (f : B -> C) (g : A -> B) (x : Select F A), *)
+(*   ((Select_map f) \o (Select_map g)) x = Select_map (f \o g) x. *)
+Obligation 2.
+extensionality x.
+simpl.
+revert b c f g.
+induction x.
+- trivial.
+- intros b c f0 g.
+  repeat rewrite Select_map_equation_2.
+  f_equal.
+  + rewrite <- fmap_comp. now rewrite IHx.
+  + admit.
+Admitted.
 
 Fixpoint Select_depth {A : Type} {F : Type -> Type}
          (x : Select F A) : nat :=
@@ -120,24 +172,146 @@ Proof.
   now rewrite Select_select_help_equation.
 Qed.
 
+Definition Select_ap {A B : Type} `{Functor F}
+           (f : Select F (A -> B)) (x : Select F A) : Select F B :=
+  Select_select (Left <$> f) (rev_f_ap <$> x).
+
+Check Select_Functor.
+
+Program Instance Select_Applicative
+        `{Functor F} : Applicative (Select F) :=
+  { is_functor := Select_Functor
+  ; pure _ x := Pure x
+  ; ap _ _ f x := Select_ap f x
+}.
+
+Program Instance Select_Selective
+        `(H : Functor F) : Selective (Select F) :=
+  { is_applicative := Select_Applicative
+  ; select _ _ x f := Select_select x f
+}.
+
+Import ApplicativeLaws.
+
+Program Instance Select_ApplicativeLaws `{Functor F} : ApplicativeLaws (Select F).
+Obligation 1.
+Admitted.
+Obligation 2.
+Admitted.
+Obligation 3.
+Admitted.
+Obligation 4.
+Admitted.
+Obligation 5.
+Admitted.
+Obligation 6.
+Admitted.
+
+(* -- F1 (Free): f <$> select x y = select (fmap f <$> x) (fmap f <$> y) *)
+(* f1 :: Selective f => (b -> c) -> f (Either a b) -> f (a -> b) -> f c *)
+(* f1 f x y = f <$> select x y === select (fmap f <$> x) (fmap f <$> y) *)
+Theorem Select_free_1 `{Functor F} :
+  forall (A B C : Type) (f : B -> C) (x : Select F (A + B)) (y : Select F (A -> B)),
+    f <$> select x y = select (fmap f <$> x)
+                              ((fun g : A -> B => f \o g) <$> y).
+Admitted.
+
+(* -- F2 (Free): select (first f <$> x) y = select x ((. f) <$> y) *)
+(* f2 :: Selective f => (a -> c) -> f (Either a b) -> f (c -> b) -> f b *)
+(* f2 f x y = select (first f <$> x) y === select x ((. f) <$> y) *)
+Theorem Select_free_2 `{Functor F} :
+  forall (A B C : Type) (f : A -> C) (x : Select F (A + B)) (y : Select F (C -> B)),
+    select (mapLeft f <$> x) y = select x ((fun g : C -> B => g \o f) <$> y).
+Admitted.
+
+(* -- F3 (Free): select x (f <$> y) = select (first (flip f) <$> x) (flip ($) <$> y) *)
+(* f3 :: Selective f => (c -> a -> b) -> f (Either a b) -> f c -> f b *)
+(* f3 f x y = select x (f <$> y) === select (first (flip f) <$> x) (flip ($) <$> y) *)
+Theorem Select_free_3 `{Functor F} :
+  forall (A B C : Type) (f : C -> A -> B)
+                        (x : Select F (A + B))
+                        (y : Select F C),
+    select x (f <$> y) = select (mapLeft (flip f) <$> x) (rev_f_ap <$> y).
+Admitted.
+
+Lemma Select_select_to_infix :
+  forall (A B : Type) `{Functor F}
+  (x : Select F (A + B)%type) (y : Select F (A -> B)),
+  Select_select x y = x <*? y.
+Proof. reflexivity. Qed.
+
+Lemma Select_map_to_fmap :
+  forall (A B : Type) `{Functor F}
+  (x : Select F A) (f : A -> B),
+  Select_map f x = fmap f x.
+Proof. reflexivity. Qed.
+
 Require Import Coq.Program.Equality.
 
 Theorem select_selective_law3_assoc
-  {A B C : Type} {F : Type -> Type} `{Functor F}
+  {A B C : Type} {F : Type -> Type} `{FunctorLaws F}
   (x : Select F (B + C))
   (y : Select F (A + (B -> C)))
   (z : Select F (A -> B -> C)) :
-  Select_select x (Select_select y z) =
-  Select_select (Select_select (Select_map law3_f x) (Select_map law3_g y)) (Select_map law3_h z).
+  x <*? (y <*? z) = (law3_f <$> x) <*? (law3_g <$> y) <*? (law3_h <$> z).
+  (* Select_select x (Select_select y z) = *)
+  (* Select_select (Select_select (Select_map law3_f x) (Select_map law3_g y)) (Select_map law3_h z). *)
 Proof.
   dependent induction z.
-  - simpl Select_map.
-    repeat rewrite Select_select_equation_1.
-    admit.
-  - repeat rewrite Select_select_equation_2.
-    simpl Select_map.
-    repeat rewrite Select_select_equation_2.
+  - remember (y <*? Pure a) as p.
+    simpl "<*?" in Heqp.
+    rewrite Select_select_equation_1 in Heqp.
+    rewrite Select_map_to_fmap in Heqp.
+    rewrite Heqp. clear Heqp p.
+    remember (fmap[ Select F] law3_h (Pure a)) as p.
+    simpl fmap in Heqp.
+    rewrite Heqp. clear Heqp p.
+    remember (fmap[ Select F] law3_f x <*? fmap[ Select F] law3_g y) as q.
+    remember ( q <*? Pure (law3_h a)) as p.
+    simpl "<*?" in Heqp.
+    rewrite Select_select_equation_1 in Heqp.
+    rewrite Select_map_to_fmap in Heqp.
+    rewrite Heqp. clear Heqp p.
+    (* rewrite Select_free_3 in Heqq. *)
+    rewrite Heqq. clear Heqq q.
+    remember (fmap[ Select F] (either (law3_h a) id)
+                  (fmap[ Select F] law3_f x <*? fmap[ Select F] law3_g y)) as p.
+    rewrite Select_free_1 in Heqp.
+    repeat rewrite fmap_comp_x in Heqp.
+    rewrite Select_free_3 in Heqp.
+    rewrite Heqp. clear Heqp p.
+    remember (x <*? fmap[ Select F] (either a id) y) as p.
+    rewrite Select_free_3 in Heqp.
+    rewrite Heqp. clear Heqp p.
     f_equal.
+    f_equal.
+    + f_equal. f_equal.
+      extensionality z. destruct z; trivial.
+    + dependent induction x.
+      * simpl fmap.
+        f_equal.
+        destruct a0; now compute.
+      * simpl fmap in *. f_equal.
+        ** admit.
+        ** unfold law3_h. unfold law3_f.
+           assert ((fun y0 : B + C => Either_map (either (uncurry a) id) (fmap[ sum B] inr y0)) =
+                   (fun y0 : B + C => fmap (either (uncurry a) id) (fmap[ sum B] inr y0))) as H1.
+           { reflexivity. }
+           rewrite H1. clear H1.
+           assert ((fun y0 : B + C => fmap (either (uncurry a) id) (fmap[ sum B] inr y0)) =
+                   (fun y0 : B + C => fmap (either (uncurry a) id \o inr) y0)) as H1.
+           { now rewrite <- fmap_comp. }
+           rewrite H1. clear H1.
+           assert ((fun y0 : B + C => fmap (either (uncurry a) id \o inr) y0) =
+                   (fun y0 : B + C => fmap id y0)) as H1.
+           { extensionality y0. destruct y0; now compute. }
+           rewrite H1. clear H1.
+           setoid_rewrite fmap_id.
+           assert ((fun k : B0 -> B + C => id \o k) = id) as H1.
+           { extensionality k. trivial. }
+           rewrite H1. clear H1.
+           rewrite fmap_id. now unfold id.
+  -
 
 
 
